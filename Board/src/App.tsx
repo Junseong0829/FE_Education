2757b2e3e0f44b948e8ed6a3a6ea10d24acacee5
Junseong Lee <kirara8029@naver.com>
2025-07-22 19:58:08 +0900

    Pokemon 심화교육 Day1

--- Board/src/App.tsx
+++ Board/src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import { useState } from 'react'
 import './App.css'
-import TodoList from "./pages/TodoList.jsx";
-import TodoList2 from "./pages/TodoList2.jsx";
-import Calculator from "./pages/Calculator";
-import Calculator2 from './pages/Calculator2';
-import Temp1 from "./pages/Temp1";
-import {CartProvider} from "./pages/CartContext";
-import ProductList from './pages/ProductList';
-import Cart from './pages/Cart';
+import Pokemon from './pages/Pokemon';
 
 function App(){
   return(
     <>
-    <CartProvider>
-      <div>
-        <ProductList />
-        <Cart />
-      </div>
-    </CartProvider>
+    <Pokemon />
     </>
   )
 }
